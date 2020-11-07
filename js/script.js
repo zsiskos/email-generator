aae5814c9063e82b95afb483edd1a4206941a136
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-11-07 07:48:02 -0500

    built

--- js/script.js
+++ js/script.js
@@ -4,29 +4,33 @@ const charSet = "abcdefghijklmnopqrstuvwxyz0123456789"
   
 //References
 let emailInputEl = document.getElementById('email-title-input')
-let emailFinal = document.getElementById('email-title-final')
+let emailDisplay = document.getElementById('email-title-final')
 let initBtn = document.getElementById('btn-init')
 let generateBtn = document.getElementById('btn-generate')
 let displayEl = document.getElementById('display')
-let sendEl = document.getElementById('send')
+let copyBtn = document.getElementById('btn-copy')
+let copyDisplayEl = document.getElementById('copy-display')
+let toolTipEl = document.getElementById('myTooltip')
+
 
 //State
-let emailLength, emailFrontLength, emailBackLength, randomNum, emailFront, warning
-displayEl.style.visibility = "hidden"
-sendEl.style.visibility = "hidden"
+let emailLength, emailFrontLength, emailBackLength, randomNum, emailFront, warning, emailFinal
+
 
 //Event Listeners
 initBtn.addEventListener('click', init)
 generateBtn.addEventListener('click', generate)
+copyBtn.addEventListener('click', copyEmail)
+copyBtn.addEventListener('mouseout', mouseOut)
 
 //Functions
 init()
 
 function init() {
   emailInputEl.value = "Enter base for email"
-  emailFinal.innerHTML = " "
+  emailDisplay.innerHTML = " "
   displayEl.style.visibility = "hidden"
-  sendEl.style.visibility = "hidden"
+  copyDisplayEl.style.visibility = "hidden"
   characterLengths();
 }
 
@@ -39,33 +43,48 @@ function characterLengths() {
 function generate(){
   generateFront(emailFrontLength)
   generateBack(emailBackLength)
+  emailFinal = (emailFront + randomNum + emailInputEl.value + randomNum + emailBack).replace(/\s+/g, "").toLowerCase()
   render()
 }
 
 function generateFront(emailFrontLength){
-  emailFront = " ";
+  emailFront = " "
   editCharSet = charSet.replace(randomNum, "")
   for (let i = 0; i < emailFrontLength; i++) {
     emailFront += editCharSet.charAt(Math.floor(Math.random() * editCharSet.length));
   }
-  return emailFront
 }
 
 function generateBack(emailBackLength){
-  emailBack = " ";
+  emailBack = " "
   editCharSet = charSet.replace(randomNum, "")
   for (let i = 0; i < emailBackLength; i++) {
     emailBack += editCharSet.charAt(Math.floor(Math.random() * editCharSet.length));
   }
-  return emailBack
+}
+
+function copyEmail(){
+  let emailArea = document.createElement("textarea")
+  document.body.appendChild(emailArea)
+  emailArea.value = emailFinal
+  emailArea.select()
+  emailArea.setSelectionRange(0, 99999)
+  document.execCommand("copy")
+  document.body.removeChild(emailArea)
+  toolTipEl.innerHTML = "Copied"
+}
+
+function mouseOut(){
+  toolTipEl.innerHTML = "Copy to clipboard"
 }
 
 function render(){
-  emailLength = emailFrontLength + emailInputEl.value.replace(/\s+/g, "").length + emailBackLength + 2
+  emailLength = (emailFinal + 2).length
   if (emailLength > maxLength) {
-    emailFinal.innerHTML = "Too long. Please shorten and try again."
+    emailDisplay.innerHTML = "Too long. Please shorten and try again."
   } else {
-    emailFinal.innerHTML = (emailFront + randomNum + emailInputEl.value + randomNum + emailBack).replace(/\s+/g, "").toLowerCase()
+    emailDisplay.innerHTML = emailFinal
+    copyDisplayEl.style.visibility = "visible"
   }
   displayEl.style.visibility = "visible"
 }
